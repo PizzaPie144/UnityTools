82721a402414d8422ec4d84beba9c2cad541f3b4
PizzaPie144 <ptitos1998@gmail.com>
2019-10-04 22:37:54 +0300

    CreateFolder, CreateAsset methods added to AssetUtils, docs updated

--- RandomTools/Assets/Public/Editor/Utility/Assets/AssetUtil.cs
+++ RandomTools/Assets/Public/Editor/Utility/Assets/AssetUtil.cs
@@ -7,13 +7,16 @@ using System.Text;
 using UnityEngine;
 using UnityEditor;
 
-namespace PizzaPie.Editor.Util.Assets
+namespace PizzaPie.Editor.Util
 {
+    /// <summary>
+    /// Contains helper methods for handling Assets
+    /// </summary>
     public static class AssetUtil
     {
 
         /// <summary>
-        /// Returns all instances of assets of type T 
+        /// Returns all instances of assets of type T.
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="folders"></param>
@@ -37,11 +40,11 @@ namespace PizzaPie.Editor.Util.Assets
         }
 
         /// <summary>
-        /// Return all instances of assets of type T, use only if GetAllInstances doesn't work
-        /// Does not work if type is contained in dll
+        /// Return all instances of assets of type T, use only if GetAllInstances doesn't work.
+        /// Does not work if type is contained in dll.
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
+        /// <returns>List of instances of assets.</returns>
         public static List<T> FindAllAssetsOfType<T>() where T : ScriptableObject
         {
             string typeGUID = "";
@@ -54,7 +57,7 @@ namespace PizzaPie.Editor.Util.Assets
 
             string targetScriptFile = "";
             //find the script file of given type
-            foreach (var f in files)                
+            foreach (var f in files)
             {
                 if (f.Replace(".cs", "").EndsWith(TType.Name) || f.Replace(".cs", "").EndsWith(TType.FullName))
                 {
@@ -166,5 +169,87 @@ namespace PizzaPie.Editor.Util.Assets
             return s;
         }
 
+        /// <summary>
+        /// Create Asset of type T.
+        /// </summary>
+        /// <typeparam name="T">Derives from ScriptableObject</typeparam>
+        /// <param name="path">Path relative to "**/MyProject/Assets/", may start with "Assets/". </param>
+        /// <param name="createPath">Create the path folders if not present. </param>
+        /// <param name="name">Name of the Asset. </param>
+        /// <returns>The new instance.</returns>
+        public static T CreateAsset<T>(string path, bool createPath = true, string name = "") where T : ScriptableObject
+        {
+            if (!path.EndsWith("/"))
+                path += '/';
+
+            if (!createPath && !AssetDatabase.IsValidFolder(path.Remove(path.Length - 1)))
+                throw new ArgumentException("Invalid path, make sure folder exists or set createPath = true");
+
+            if (createPath)
+                CreateFolder(path);
+
+            AssetDatabase.Refresh();
+
+            T instance = ScriptableObject.CreateInstance<T>();
+
+            if (name.Equals(""))
+                name = typeof(T).Name;
+
+            int count = 0;
+
+            var files = Directory.GetFiles(path);
+
+            foreach (var file in files)
+            {
+                string f = file.Replace(path, "");
+                if (f.Contains(name) && f.Contains(".asset") && !f.Contains(".meta"))
+                    count++;
+            }
+
+            if (count != 0)
+                name += " " + count.ToString();
+
+            AssetDatabase.CreateAsset(instance, path + name + ".asset");
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+
+            return instance;
+        }
+
+        /// <summary>
+        /// Create folder/s based on path if they do not exist.
+        /// </summary>
+        /// <param name="path">Relative path to "**/MyProject/Assets/", may start with "Assets/".</param>
+        /// <code><para>CreateFolder("newFolder/newFolder") </para>
+        /// <para>CreateFolder("Assets/newFolder/newFolder") </para></code>
+        public static string CreateFolder(string path)
+        {
+            string[] sep = path.Split('/');
+            string p = "Assets/";
+            int i = 0;
+
+            if (path.StartsWith("Assets/"))
+                i = 1;
+
+            string lastValidPath = "";
+
+            for (; i < sep.Length; i++)
+            {
+                if (i == sep.Length - 1 && sep[i].Equals(""))
+                    continue;
+
+                p = String.Concat(p, sep[i]);
+                if (!AssetDatabase.IsValidFolder(p))
+                {
+                    AssetDatabase.CreateFolder(lastValidPath, sep[i]);
+                    lastValidPath = lastValidPath + '/' + sep[i];
+                }
+
+                lastValidPath = p;
+                p = String.Concat(p, '/');
+            }
+
+            return p;
+        }
     }
 }
